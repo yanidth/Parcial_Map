93bc860260753ce4d60363d8fa1b041a7ca62a4e
Yanidth Garzón <yanidthfernanda@gmail.com>
2025-02-28 19:27:31 -0500

    v1

--- program.js
+++ program.js
@@ -4,3 +4,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+
+async function loadPolygon() {
+    
+    let myDates = await fetch('barrios_prueba.geojson');
+    let myPolygon = await myDates.json();
+    
+    L.geoJSON(myPolygon,
+        {
+            style:{
+
+                color="blue"
+
+            }
+
+        }
+    
+    )
+
+}
+
+loadPolygon();
