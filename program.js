8f1e35a302374383bce8d35bdfa3f4eeb7822db0
Yanidth Garzón <yanidthfernanda@gmail.com>
2025-02-28 19:02:30 -0500

    v0

---
+++ program.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.6920279795640125, -74.07692210386752], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
