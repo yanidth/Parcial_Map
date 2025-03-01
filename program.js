c368702f4205857ee8155b3d8b8877a5508bf28b
yanidth <yanidthfernanda@gmail.com>
2025-03-01 12:24:30 -0500

    v4

--- program.js
+++ program.js
@@ -5,8 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
-
 async function loadPolygon() {
     
     let myDates = await fetch('barrios_prueba.geojson');
@@ -30,5 +28,84 @@ loadPolygon();
 
 let btnTrees = document.getElementById("btnTrees");
 
-btnTrees.addEventListener("click", ()=> alert('Hola'));
+btnTrees.addEventListener("click",
+    async ()=>{
+        let response = await fetch('arboles_La_Alborada.geojson');
+        let datos = await response.json();
+
+        //Agregar la capa al mapa
+        L.geoJSON(
+            datos,
+            {
+                pointToLayer: function(feature, latlng){
+
+                    return L.circleMarker(latlng, {
+                        radius: 5,
+                        fillColor: "green",
+                        weight: 1,
+                        opacity: 1,
+                        fillOpacity: 0.8
+                    })
+   
+                }
+            }
+        ).addTo(map);
+    }
+)
+
+let btnDistance = document.getElementById("btnDistance");
+
+btnDistance.addEventListener("click",
+    async ()=>{
+
+        let response = await fetch('arboles_La_Alborada.geojson');
+        let datos = await response.json();
+        let trees = datos.features.map((myElement, index) => ({
+            id: index + 1,
+            coordinates: myElement.geometry.coordinates
+        }));
+
+        let distances = [];
+
+        trees.forEach((treeA) => {trees.forEach(
+                (treeB)=>{
+                    //Calcular la distancia de treeA a cada uno de los arboles.
+                    if(treeA.id != treeB.id){                    
+                        let distance = turf.distance(
+                            turf.point(treeA.coordinates),
+                            turf.point(treeB.coordinates)
+
+                        );
+
+                        distances.push(
+                            [
+                                `Arbol ${treeA.id}`,
+                                `Arbol ${treeB.id}`,
+                                distance.toFixed(3)
+                            ]
+                        )
+                }   
+            }
+            )
+        }
+        )
+        generatePDF(distances, trees.length);
+    }
+)
+function generatePDF(distances, totalTrees){ 
+
+    let {jsPDF} = window.jspdf;
+    let documentPDF =new jsPDF();
+
+    documentPDF.text('Distancias entre arboles del Barrio La Alborada', 10, 10);
+    
+    documentPDF.autoTable(
+        {
+            head: [['Árbol 1', 'Arbol 2', 'Distancia (km)']],
+            body: distances
+        }
+
+    );
+    documentPDF.save('Distancias_Árboles_La_Alborada.pdf');
+}
 
